722c20706e0d37535d5f451a868df24a2a36997c
mukunzidd <dmukunzi0@gmail.com>
2022-12-14 16:19:30 +0200

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './app';
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  console.log(process.env);
+  /* eslint-enable no-console */
+});
